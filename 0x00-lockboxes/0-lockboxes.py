9e1256db050d4695ceac716da92b2d626c09f43e
ronald0204 <ronaldaguirre0204@gmail.com>
2022-04-21 21:38:27 -0500

    all files

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    unlocked = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
